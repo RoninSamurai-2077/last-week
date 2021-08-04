9bdb324666f6f0a094fd47ddf7f3d881490b6d2c
Greg <Gdixon5125@gmail.com>
2021-08-04 16:44:03 -0400

    mmm

--- Coding Exercises - even_sum().js
+++ Coding Exercises - even_sum().js
@@ -1,13 +1,13 @@
 // Write a function called even_sum() that takes an array of numbers and returns the sum of values in the array that are even numbers.
 // Examples:
-Input: [2, 1, 6, 5, 3, 2, 4 ]
-Output: 14
+// Input: [2, 1, 6, 5, 3, 2, 4 ]
+// Output: 14
 
-Input: [1, 5, 7, 9]
-Output: 0
+// Input: [1, 5, 7, 9]
+// Output: 0
 
-Input: [2, 4, 6, 3]
-Output: 12
+// Input: [2, 4, 6, 3]
+// Output: 12
 let input = [2, 5, 8, 10, 25, 6, 4]
 let output = sum(input)
 function even_sum(i) {
