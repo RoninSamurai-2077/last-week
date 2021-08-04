f5ceac0344a533d1bda2fbafc095265f1713ef9e
Greg <Gdixon5125@gmail.com>
2021-08-04 16:36:53 -0400

    new

---
+++ .js
@@ -0,0 +1,22 @@
+// Write a function called even_sum() that takes an array of numbers and returns the sum of values in the array that are even numbers.
+// Examples:
+Input: [2, 1, 6, 5, 3, 2, 4 ]
+Output: 14
+
+Input: [1, 5, 7, 9]
+Output: 0
+
+Input: [2, 4, 6, 3]
+Output: 12
+let input = [2, 5, 8, 10, 25, 6, 4]
+let output = sum(input)
+function even_sum(i) {
+    for (i=0; i < input.length; i++) {
+        if (i%2 == 0) {
+        //even
+        }else {
+            // nothing
+        }
+    };
+    return sum(input)
+};
